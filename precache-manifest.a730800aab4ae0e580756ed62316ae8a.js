2152723b36625c546a6fd58bea9ba7077033fdf3
Minori Hashimoto <minori.fh@gmail.com>
2019-08-06 11:52:57 -0700

    Updates

---
+++ precache-manifest.a730800aab4ae0e580756ed62316ae8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3616f87c20053ff2a09e673acef209",
+    "url": "/jumbleClient/index.html"
+  },
+  {
+    "revision": "f9b0d627751450e16a94",
+    "url": "/jumbleClient/static/css/main.fa4217cc.chunk.css"
+  },
+  {
+    "revision": "2b6ee5928d8287f34fff",
+    "url": "/jumbleClient/static/js/2.80954dbe.chunk.js"
+  },
+  {
+    "revision": "f9b0d627751450e16a94",
+    "url": "/jumbleClient/static/js/main.78b1788f.chunk.js"
+  },
+  {
+    "revision": "e228814ca77279b28bf4",
+    "url": "/jumbleClient/static/js/runtime~main.8cfe0ae2.js"
+  },
+  {
+    "revision": "a6ec563bcc7bfe131ea1976cb17b4915",
+    "url": "/jumbleClient/static/media/jumble.a6ec563b.gif"
+  }
+]);
